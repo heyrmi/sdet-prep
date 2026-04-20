164a8a997d202dabbb75caacbd2dfb873cc6fc19
Rahul Mishra <rahulmishra.kv@gmail.com>
2026-04-20 10:52:17 +0530

    change to Ques3_StringDecompression

--- dsa/src/main/java/ra/hul/dsa/strings/Ques3_StringDecompression.java
+++ dsa/src/main/java/ra/hul/dsa/strings/Ques3_StringDecompression.java
@@ -5,20 +5,20 @@ public class Ques3_StringDecompression {
         if (str == null || str.isEmpty()) return str;
 
         StringBuilder sb = new StringBuilder();
-        int i = 0;
+        int read = 0;
 
-        while (i < str.length()) {
+        while (read < str.length()) {
             int count = 0;
 
-            while (i < str.length() && Character.isDigit(str.charAt(i))) {
-                count = count * 10 + (str.charAt(i) - '0'); // handling multiple digits
-                i++;
+            while (read < str.length() && Character.isDigit(str.charAt(read))) {
+                count = count * 10 + (str.charAt(read) - '0'); // handling multiple digits
+                read++;
             }
 
             // Append character n times
-            if (i < str.length()) {
-                sb.append(String.valueOf(str.charAt(i)).repeat(count));
-                i++;
+            if (read < str.length()) {
+                sb.append(String.valueOf(str.charAt(read)).repeat(count));
+                read++;
             }
         }
         return sb.toString();
